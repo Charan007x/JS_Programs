dcf6e4b45bfa9b272a881b840517f74d40a26dd6
Charan007x <saicharangottipalli@gmail.com>
2025-11-08 12:06:44 +0530

    New Commit

--- p1.js
+++ p1.js
@@ -1,4 +1,6 @@
-let id=12345;
-console.log(`ID is :${id}`);
-id=54321;
-console.log(`Updated ID is :${id}`);
+let name="Charan";
+let age=20;
+let isStudent = true;
+console.log(`My name is ${name}`);
+console.log(typeof(age));
+console.log(typeof(isStudent));
